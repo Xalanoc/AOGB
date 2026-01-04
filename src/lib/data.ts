f33eb4907c72fae8f35d3ad7e0a6da298efb0179
Voltaction <jaesaebag@gmail.com>
2026-01-04 17:05:58 +0900

    Update data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,7 +2,7 @@ export interface Item {
   name: string;
   type: 'helmet' | 'chestplate' | 'boots' | 'amulet' | 'grease' | 'potion' | 'blood disease' | 'aura/focus' | 'fightingStyle';
   image: string;
-  stats?: Stats;  // Sailor는 stats 없을 수 있음
-  effects?: string[];  // 동적 효과 설명 배열 (선택)
+  stats?: Stats;  
+  effects?: string[];  
   rarity?: string;
 }
